edff68922172bbf8e287c193ba2d2f50c70b664f
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-09-28 17:57:45 +0200

    for validation

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -4,6 +4,7 @@ const loginUser = require("./loginUser");
 const logoutUser = require("./logoutUser");
 const getCurrentUser = require("./getCurrentUser");
 const updateUserInfo = require("./updateUserInfo");
+const tasks = require("./tasks");
 
 module.exports = {
   registerUser: ctrlWrapper(registerUser),
--- controllers/auth/tasks.js
+++ controllers/auth/tasks.js
@@ -1,32 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-app.get("/tasks", (req, res) => {
-    const month = req.query.month;
-    
-    if (!month) {
-      res.status(400).json({ message: "Параметр 'month' є обов'язковим" });
-      return;
-    }
-
-    const tasksCollection = generateTasksForMonth(month);
-
-     res.json(tasksCollection);
-});
-
-function generateTasksForMonth(month) {
-
-    const tasksForMonth = [
-      { id: 1, title: "Task 1", month: month },
-      { id: 2, title: "Task 2", month: month },
-      { id: 3, title: "Task 3", month: month },
-    ];
-   return tasksForMonth;
-};
-
 const tasks = [];
 
-app.post("/tasks", (req, res) => {
+router.get("/tasks", (req, res) => {
+  const tasksForMonth = [
+    { id: 1, title: "Task 1", month: month },
+    { id: 2, title: "Task 2", month: month },
+    { id: 3, title: "Task 3", month: month },
+  ];
+  res.json(tasksForMonth);
+});
+
+router.post("/tasks", (req, res) => {
   const newTask = req.body;
 
   tasks.push(newTask);
@@ -34,7 +20,7 @@ app.post("/tasks", (req, res) => {
   res.json(newTask);
 });
 
-app.patch("/tasks/:id", (req, res) => {
+router.patch("/tasks/:id", (req, res) => {
   const taskId = parseInt(req.params.id);
   const updatedData = req.body;
 
@@ -49,20 +35,15 @@ app.patch("/tasks/:id", (req, res) => {
   }
 });
 
-app.delete("/tasks/:id", (req, res) => {
-
+router.delete("/tasks/:id", (req, res) => {
   const taskId = parseInt(req.params.id);
   const taskIndex = tasks.findIndex((task) => task.id === taskId);
 
-  
   if (taskIndex !== -1) {
-
     tasks.splice(taskIndex, 1);
-
 
     res.status(204).send();
   } else {
-  
     res.status(404).json({ message: "Task not found" });
   }
 });
