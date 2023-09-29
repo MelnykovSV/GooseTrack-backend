b0ba8f6d07f6e47be438e5ba2796695d52f30a6f
MelnykovSV <svmelnykov@gmail.com>
2023-09-29 16:12:07 +0300

    1

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const ctrl = require("../../controllers/reviews");
-const auth = require("../../middlewares/authenticate");
+const { authenticate } = require("../../middlewares/");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAllReviews);
-router.get("/userReview", auth, ctrl.getUserReviews);
-router.post("/userReview", auth, ctrl.addReview);
-router.patch("/userReview", auth, ctrl.updateUserReviews);
-router.delete("/userReview", auth, ctrl.deleteUserReviews);
+router.get("/userReview", authenticate, ctrl.getUserReviews);
+router.post("/userReview", authenticate, ctrl.addReview);
+router.patch("/userReview", authenticate, ctrl.updateUserReviews);
+router.delete("/userReview", authenticate, ctrl.deleteUserReviews);
 
 module.exports = router;
